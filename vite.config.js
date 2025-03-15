42625ce5b2810a560e387b7973ee837c24265c22
kalaivanan <kalaivkv24@gmail.com>
2025-03-15 09:41:22 +0530

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/Website/",
-  plugins: [react(),  tailwindcss(),],
-  
+  base: "./",
+  build: {
+    outDir: "dist", 
+  },
+  plugins: [react(), tailwindcss()],
 });
